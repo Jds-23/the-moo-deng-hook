dc6e311c1bf0a0dbbe0d005ef286ee2813b90350
Joydeep <joydeepsingha68@gmail.com>
2024-11-17 08:30:29 +0700

    rebase changes

--- utils/UnichainSepoliaDeployments.sol
+++ utils/UnichainSepoliaDeployments.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.0;
 
 library UnichainSepoliaDeployments {
     address public constant manager =
-        0xC81462Fec8B23319F288047f8A03A57682a35C1A;
+        0x7Cb22e8cfBdaCd1bC12Afe63ac0B561785d8a976;
+    // address public constant manager =
+    //     0xC81462Fec8B23319F288047f8A03A57682a35C1A; OG reverting for some reason
     address public constant token0 = 0x714A8B5821b8fBD7148C6A7f80A8239D5B9802B8;
     address public constant token1 = 0x75741A5766cb7A944E19609892812ED0A6990951;
+    address public constant theHook =
+        0x396Ed0A95D63A11C98Ca075f67A04f9C0772A080;
     address public constant pythContract =
         0x2880aB155794e7179c9eE2e38200202908C17B43;
     bytes32 public constant priceFeedId =
